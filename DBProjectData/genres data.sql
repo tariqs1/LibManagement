e118b6ceb793db075117a155c12475bb7dc7754f
Kretsch-Matt <42364731+Kretsch-Matt@users.noreply.github.com>
2025-05-06 13:51:46 -0500

    Added Data folder

---
+++ DBProjectData/genres data.sql
@@ -0,0 +1,29 @@
+
+--
+-- Dumping data for table `genres`
+--
+
+LOCK TABLES `genres` WRITE;
+/*!40000 ALTER TABLE `genres` DISABLE KEYS */;
+INSERT INTO `genres` VALUES
+(1,'asperiores','Cum consequatur porro accusantium incidunt et autem minima. Enim et ipsa aperiam quisquam omnis laborum omnis voluptas. Fugit temporibus voluptates delectus eius omnis quae saepe. Esse rerum provident odio doloremque.'),
+(2,'est','Repellendus vitae dignissimos perferendis cupiditate facere consequuntur. Sunt non laboriosam fugit voluptatibus sed. Laudantium rerum voluptates autem. Voluptatem ut ab voluptatem et perferendis qui modi.'),
+(3,'sed','Porro omnis quis nihil hic numquam itaque quis. Temporibus eveniet est accusamus ut dolores id et officiis.'),
+(4,'et','Officiis officia officia omnis. Dolores occaecati provident mollitia sunt ut. Et ut aspernatur quisquam totam et consequatur autem.'),
+(5,'earum','Ut et aut consequatur consectetur et excepturi. Doloremque consequuntur nostrum at recusandae. Id aut id in distinctio eos et nam.'),
+(6,'ea','In enim officia accusamus. Quidem ratione voluptate et quam eveniet perferendis. Nam rerum nesciunt ut.'),
+(7,'cupiditate','Culpa sequi aut quasi eveniet eveniet voluptatum. Aliquid hic quidem consequuntur dolore ex. Qui sit quos magni non quia.'),
+(8,'mollitia','Provident aspernatur dolores et nostrum. Rem dolore sit quisquam quis. Consequuntur corporis animi sequi facilis dolorem ducimus quasi. Sequi ullam consequatur et doloremque.'),
+(9,'aperiam','At suscipit quo est qui sed. Quo totam consectetur voluptas. Dolor ratione molestiae deleniti ex id id. Aliquam dignissimos pariatur voluptatem ut distinctio nostrum quia.'),
+(10,'autem','Est et voluptatem eveniet. Commodi non vitae optio laborum alias ratione quisquam. Architecto illum cumque ut qui porro error neque velit. Quia commodi ad ut qui earum enim repellat. Nisi a sequi tempora cumque.');
+/*!40000 ALTER TABLE `genres` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ DBProjectData/publishers data.sql
@@ -0,0 +1,40 @@
+
+--
+-- Dumping data for table `publishers`
+--
+
+LOCK TABLES `publishers` WRITE;
+/*!40000 ALTER TABLE `publishers` DISABLE KEYS */;
+INSERT INTO `publishers` VALUES
+(1,'Homenick Ltd','4483 Jessica Isle\nPort Selenaside, NJ 82536','247-365-8541x52429','hodkiewicz.jayme@example.net','http://www.luettgen.info/',1),
+(2,'Haley Group','9284 Feeney Islands Suite 128\nNorth Fredside, NY 64684','091.394.4341x89599','parker77@example.org','http://bogisich.com/',2),
+(3,'Emard, Steuber and Renner','73879 Keebler Trail\nEast Eddburgh, MT 52658','882.703.1935','marina.gottlieb@example.org','http://ferry.com/',3),
+(4,'Anderson-Von','457 Manuela Stravenue\nTreyborough, AK 12459','609-170-6126x68017','cole.pat@example.com','http://towne.com/',4),
+(5,'Hills, Runolfsdottir and Wilderman','282 Tremblay Estates Apt. 649\nAntwanfort, UT 13079','(226)569-8418x0691','creola.leannon@example.com','http://harvey.info/',5),
+(6,'McKenzie and Sons','08896 Kuvalis Ramp Suite 565\nEast Arvel, HI 90705','1-589-307-5306x9039','zachery.padberg@example.net','http://www.ullrichpadberg.com/',6),
+(7,'Volkman, Considine and Cartwright','90481 Alexandria Heights Suite 212\nWelchfurt, SD 42780','(040)414-8837x361','edmund73@example.org','http://www.pfefferstiedemann.org/',7),
+(8,'Jones, Wisozk and Raynor','811 Isadore Prairie Apt. 833\nGailshire, ID 50891','456.056.7023x557','frederick31@example.com','http://mclaughlin.com/',8),
+(9,'Bahringer, Schmeler and Schmitt','7732 Cheyanne Crest Apt. 555\nBrakusfurt, AK 81266','(094)370-1668x6982','reinger.jasmin@example.com','http://greenfelder.com/',9),
+(10,'Collier, Strosin and Predovic','236 Quincy Stream Suite 155\nNew Juston, AK 32990','1-819-273-7700','bins.ethyl@example.com','http://carrolllesch.com/',10),
+(11,'Konopelski LLC','14815 Destiny Station Suite 180\nWuckertborough, TN 11094','1-937-767-2078x2682','rahul.moen@example.org','http://www.hartmannmosciski.com/',11),
+(12,'Haag-Russel','649 Daren Shoals Apt. 440\nNorth Annettaville, UT 15326-1071','1-586-114-1972x259','corkery.madge@example.org','http://considine.com/',12),
+(13,'Prosacco, Ullrich and Boehm','569 Joey Trail\nYostburgh, SC 40572-8195','(767)260-7737','eebert@example.net','http://www.dooley.org/',13),
+(14,'Kovacek-Hahn','28792 Keith Creek\nLake Savannah, SC 88371-3457','1-395-763-5753x41468','mathias.volkman@example.org','http://ullrichpurdy.com/',14),
+(15,'Lang Inc','14000 Schowalter Garden Apt. 087\nNew Domenica, NC 00425-1961','687.984.3664x844','bechtelar.clementine@example.org','http://nitzsche.com/',15),
+(16,'Nader Ltd','9859 Mark Square Apt. 598\nSouth Warrenshire, OR 15588','104-269-5607x943','kiera.turner@example.com','http://www.colenienow.com/',16),
+(17,'Lowe, Beatty and Konopelski','72293 Oma Rapids Suite 669\nYostburgh, CO 83179','072.734.6505','bertram.von@example.com','http://www.ankunding.com/',17),
+(18,'Little, Morar and Veum','23826 Rhiannon Divide\nEast Chelsie, VA 24244','(726)360-5181x512','zane67@example.com','http://purdy.com/',18),
+(19,'Rogahn, Roob and Schinner','1833 Rebeka Shore Apt. 414\nNicholehaven, MT 42938-9966','557-886-9774x44863','rogahn.caitlyn@example.com','http://www.murray.com/',19),
+(20,'Terry LLC','6952 Vandervort Forest Apt. 522\nNew Otto, CA 98881','+87(8)1338233409','douglas.roslyn@example.com','http://mertz.com/',20);
+/*!40000 ALTER TABLE `publishers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
